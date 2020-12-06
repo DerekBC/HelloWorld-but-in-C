28fa2e24392f824c543bbcdba75c5a07d1cefd43
DerekBC <70551371+DerekBC@users.noreply.github.com>
2020-12-06 16:18:13 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Me:I am good in C language");
+		System.out.println("Interviewer:Then write Hello World using C");
+		System.out.println("Me:");
+		System.out.println("C   C  CCCCC  C      C      CCCCC\r\n" + 
+				"C   C  C      C      C      C   C\r\n" + 
+				"C   C  C      C      C      C   C\r\n" + 
+				"CCCCC  CCC    C      C      C   C\r\n" + 
+				"C   C  C      C      C      C   C\r\n" + 
+				"C   C  C      C      C      C   C\r\n" + 
+				"C   C  CCCCC  CCCCC  CCCCC  CCCCC\r\n" + 
+				"\r\n" + 
+				"C   C  CCCCC  CCCCC  C      CCCC\r\n" + 
+				"C   C  C   C  C   C  C      C   C\r\n" + 
+				"C C C  C   C  CCCCC  C      C   C\r\n" + 
+				"CC CC  C   C  CC     C      C   C\r\n" + 
+				"C   C  C   C  C C    C      C   C\r\n" + 
+				"C   C  C   C  C  C   C      C   C\r\n" + 
+				"C   C  CCCCC  C   C  CCCCC  CCCC");
+	}
+}
